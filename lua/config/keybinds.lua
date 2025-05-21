a94fcaeba990ca2199d2424fded58b5d6672c9b4
Oleksii Teterin <altmoc@gmail.com>
2025-05-21 22:37:47 +0200

    Update keybindings

--- lua/config/keybinds.lua
+++ lua/config/keybinds.lua
@@ -1,2 +1,24 @@
+local opts = { noremap = true, silent = true }
+
 vim.g.mapleader = " "
+vim.g.maplocalleader = " "
+
 vim.keymap.set("n", "<leader>n", vim.cmd.Ex)
+
+vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv", { desc = "moves lines down in visual selection" })
+vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv", { desc = "moves lines up in visual selection" })
+
+vim.keymap.set("n", "n", "nzzzv")
+vim.keymap.set("n", "N", "Nzzzv")
+
+vim.keymap.set("v", "<", "<gv", opts)
+vim.keymap.set("v", ">", ">gv", opts)
+
+-- Paste without replacing clipboard content
+vim.keymap.set("x", "<leader>p", [["_dP]])
+vim.keymap.set("v", "p", '"_dp', opts)
+vim.keymap.set({ "n", "v" }, "<leader>d", [["_d]])
+
+vim.keymap.set("i", "<C-c>", "<Esc>")
+vim.keymap.set("n", "<C-c>", ":nohl<CR>", { desc = "Clear search hl", silent = true })
+vim.keymap.set("n", "x", '"_x', opts) -- prevents deleted characters from copying to clipboard
--- lua/plugins/colors.lua
+++ lua/plugins/colors.lua
@@ -1,17 +1,27 @@
 return {
     {
-	"folke/tokyonight.nvim",
-	config = function()
-	    vim.cmd([[colorscheme tokyonight]])
-	end,
+        "folke/tokyonight.nvim",
+        config = function()
+            vim.cmd([[colorscheme tokyonight]])
+        end,
     },
     {
-	"nvim-lualine/lualine.nvim",
-	dependencies = {
-	    "nvim-tree/nvim-web-devicons",
-	},
-	opts = {
-	    theme = "tokyonight",
-	}
+        "rose-pine/neovim",
+        name = "rose-pine",
+        config = function()
+            require("rose-pine").setup({
+                variant = "main",
+                dark_variant = "main",
+            })
+        end,
+    },
+    {
+        "nvim-lualine/lualine.nvim",
+        dependencies = {
+            "nvim-tree/nvim-web-devicons",
+        },
+        opts = {
+            theme = "tokyonight",
+        }
     }
 }
